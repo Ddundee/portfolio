4da8eee84578942fa8d6c86bb9897e49d3c042c7
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2022-11-22 22:33:06 -0600

    Added skillset tab

---
+++ app/json/data.js
@@ -0,0 +1,9 @@
+export default data = {
+    name: ["Dhanush", "Reddy", "Chilakala"],
+    description: "A half-assed 16 year-old UI/UX Designer, Fullstack Developer, and Certified Video Designer based near Dallas, Texas.",
+    skillset: {
+        applications: ["Adobe Photoshop", "Adobe Premier Pro", "Adobe XD"],
+        languages: [],
+        Applications
+    }
+}
