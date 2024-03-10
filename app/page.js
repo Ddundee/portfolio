c2da02efad618ea8765db8628ff839280f07bad7
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2024-03-10 03:16:01 -0500

    finished loading slash screen

--- app/page.js
+++ app/page.js
@@ -3,24 +3,60 @@
 import { useEffect, useRef, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import QuoteCard from "./components/quoteCard";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const [pageLoaded, setPageLoaded] = useState(false);
+  const [loadingPercentage, setLoadingPercentage] = useState(0);
 
   useEffect(() => {
-    handleToastDismiss();
-    setPageLoaded(true)
-  }, [])
+    const updateLoadingPercentage = () => {
+      const { loadEventEnd, navigationStart } = performance.timing;
+      const pageLoadTime = loadEventEnd - navigationStart;
+      const percentage = Math.min(
+        Math.floor((performance.now() / pageLoadTime) * 100),
+        100
+      );
+      setLoadingPercentage(percentage);
 
-  if(!pageLoaded) return <Loading />;
-  else return (
-    <div>Hello</div>
-  )
+      if (percentage === 100) {
+        setTimeout(() => {
+          handleToastDismiss();
+          setPageLoaded(true);
+        },);
+      } else {
+        requestAnimationFrame(updateLoadingPercentage);
+      }
+    };
+
+    requestAnimationFrame(updateLoadingPercentage);
+  }, []);
+
+  if (!pageLoaded) return <Loading percentage={loadingPercentage} />;
+  else
+    return (
+      <div className="w-full h-full flex justify-center align-middle">
+        <Toaster
+        position="bottom-left"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            background: "none",
+            boxShadow: "none",
+            padding: "0",
+            margin: "0",
+            border: "none",
+          },
+        }}
+      />
+        <div>Hello</div>
+      </div>
+    );
 }
 
-let handleToastDismiss
+let handleToastDismiss;
 
-function Loading() {
+function Loading({ percentage }) {
   const toastRef = useRef(null);
 
   const handleDismiss = () => {
@@ -28,7 +64,7 @@ function Loading() {
       toast.dismiss(toastRef.current);
       toastRef.current = null;
     }
-  }
+  };
 
   useEffect(() => {
     if (toastRef.current === null) {
@@ -36,27 +72,49 @@ function Loading() {
         duration: Infinity,
       });
     }
-    
+
     handleToastDismiss = handleDismiss;
     return handleDismiss;
   }, []);
+
   return (
-    <div>
+    <div className="font-geist_sans">
       <Toaster
         position="bottom-left"
         reverseOrder={false}
         toastOptions={{
           style: {
-            background: 'none',
-            boxShadow: 'none',
-            padding: '0',
-            margin: '0',
-            border: 'none',
+            background: "none",
+            boxShadow: "none",
+            padding: "0",
+            margin: "0",
+            border: "none",
           },
         }}
-
       />
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div>
+          <ProgressBar percentage={percentage} />
+          <div className="w-full h-[14px]" />
+          <div className="flex justify-center text-[#170202] font-normal text-xl">
+            <p>{percentage}%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
+function ProgressBar({ percentage }) {
+  return (
+    <div className="w-[579px]">
+      <motion.div
+        initial={{ x: 0 }}
+        animate={{ x: (percentage / 100) * 531 }}
+        transition={{ ease: "linear", duration: 0.5 }}
+        className="w-12 h-12 rounded-full border-[#170202] border-[3px]"
+      />
+      <div className="h-[3px] bg-[#170202] w-[579px] rounded-full" />
     </div>
   );
 }
