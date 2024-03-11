521203d0c8180a4bce7528551b9029ccaa286cc0
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2024-03-11 02:27:18 -0500

    fixed bug

--- app/components/projectCard.js
+++ app/components/projectCard.js
@@ -1,7 +1,6 @@
 
 "use client";
 import React, { useEffect, useState } from "react";
-import Button from "@/app/components/button";
 import Skeleton from "./skeleton";
 import { Toaster } from "react-hot-toast";
 
--- app/components/quoteCard.js
+++ app/components/quoteCard.js
@@ -1,7 +1,6 @@
 
 "use client";
 import { useEffect, useState } from "react";
-import Button from "@/app/components/button";
 import Skeleton from "./skeleton";
 import { Toaster } from "react-hot-toast";
 
