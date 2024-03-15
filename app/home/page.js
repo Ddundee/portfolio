a9d784c3ad3c7970228dd6e5844738e20592341e
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2024-03-15 12:09:54 -0500

    # Please include a useful commit message!

--- app/home/page.js
+++ app/home/page.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import PageTemplate from './../../components/pageTemplate';
+import PageTemplate from '../../components/pageTemplate';
 import React from 'react';
 
 /**
