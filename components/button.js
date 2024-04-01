3b5a0345c3ac9471bf86b11cb26e2259829edd8f
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2024-03-31 21:25:13 -0500

    # Please include a useful commit message!

--- components/button.js
+++ components/button.js
@@ -1,14 +1,8 @@
-'use client';
-export default function Button({action, children}) {
-  if (!action) action = () => {};
-
+export default function Button({children}) {
   return (
     <div className='w-max h-max hover:cursor-pointer select-none'>
       <div
         className={`px-4 py-2 bg-[#170202] text-white rounded-[6px] duration-100 origin-center active:scale-95 hover:opacity-70`}
-        onClick={() => {
-          action();
-        }}
       >
         {children}
       </div>
