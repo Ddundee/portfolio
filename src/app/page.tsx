c1a19b8fa2ff4e43b6a81a901a5b452620cf8964
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2024-12-20 07:10:39 -0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 export default function Home() {
     return (
-        <div>a</div>
+        <div className="px-3 p-12 flex flex-col items-center h-screen">
+            <div className="max-w-screen-sm w-full space-y-6">
+                <h1 className="font-medium text-lg">Dhanush Chilakala</h1>
+                <p className="">I am a student at Virginia Tech, majoring in Computer Science & Data Science. I am also a frontend developer who codes dumb things in my free time.</p>
+                <div>
+                    <h2>Things I&apos;ve worked on recently:</h2>
+                    <ul className="list-disc list-inside">
+                        <li>
+                            <a href="https://simply-invite.vercel.app/" target="_blank">Simply Invite</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex gap-3">
+                    <a href="https://drive.google.com/file/d/1OAMRp2yjhf_e_t18RP0Ccc-ZOFM61G_s/view?usp=sharing" target="_blank">Resume</a>
+                    <a href="https://github.com/Ddundee" target="_blank">Github</a>
+                    <a href="https://www.linkedin.com/in/dhanush-chilakala/" target="_blank">LinkedIn</a>
+                </div>
+            </div>
+        </div>
     );
 }
