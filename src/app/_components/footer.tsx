e0c89d3d519d24bb332bb484a0c1134dc15ac337
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2025-04-07 04:11:02 -0400

    hides secret

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
             <a className="no-underline text-stone-400 hover:text-orange-600 py-1 px-2 rounded-sm hover:bg-orange-300/5" target="_blank" href="https://www.github.com/ddundee">github</a>
             <a className="no-underline text-stone-400 hover:text-orange-600 py-1 px-2 rounded-sm hover:bg-orange-300/5" target="_blank" href="https://www.linkedin.com/in/dhanush-chilakala/">linkedin</a>
             {/* <Link className="no-underline text-stone-400 hover:text-orange-600 py-1 px-2 rounded-sm hover:bg-orange-300/5" href="/contact">or just fill out this form</Link> */}
-            <p className="no-underline cursor-default">krishna likes minors</p>
+            <p className="no-underline cursor-default select-none">krishna likes minors</p>
         </footer>
     )
 }
